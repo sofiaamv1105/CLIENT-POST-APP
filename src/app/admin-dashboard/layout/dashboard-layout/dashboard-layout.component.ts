34c6722101d2b1f16027688dbe349b793926119f
Sofia <sofiaamv2003@gmail.com>
2025-05-15 22:23:16 -0500

    Implementacion de Gestion de Usuarios

--- src/app/admin-dashboard/layout/dashboard-layout/dashboard-layout.component.ts
+++ src/app/admin-dashboard/layout/dashboard-layout/dashboard-layout.component.ts
@@ -1,10 +1,10 @@
 import { Component, Inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { AuthService } from '../../../auth/services/auth.service';
 
 @Component({
   selector: 'app-dashboard-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './dashboard-layout.component.html',
   styleUrl: './dashboard-layout.component.css'
 })
