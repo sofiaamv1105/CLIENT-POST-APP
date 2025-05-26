646ca4759e64794c906887d147b61a78f2619c84
Sofia <sofiaamv2003@gmail.com>
2025-05-25 19:57:19 -0500

    avances de dashboard

--- src/app/admin-dashboard/layout/dashboard-layout/dashboard-layout.component.ts
+++ src/app/admin-dashboard/layout/dashboard-layout/dashboard-layout.component.ts
@@ -1,5 +1,5 @@
-import { Component, Inject } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Component, inject, Inject } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AuthService } from '../../../auth/services/auth.service';
 
 @Component({
@@ -9,8 +9,11 @@ import { AuthService } from '../../../auth/services/auth.service';
   styleUrl: './dashboard-layout.component.css'
 })
 export class DashboardLayoutComponent {
-onImgError($event: ErrorEvent) {
-throw new Error('Method not implemented.');
-}
-  constructor(public authService: AuthService) {}
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  logout() {
+    this.authService.logout();
+    this.router.navigateByUrl('/auth/login');
+  }
 }
